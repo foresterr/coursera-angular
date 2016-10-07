67e58cddc9f9d19bed9619cd87cce2f4bec73be9
Kuba Michalik <foresterr@gmailom>
2016-10-07 13:48:21 +0200

    Add: final solution

--- module2/js/app.js
+++ module2/js/app.js
@@ -8,12 +8,10 @@
   function ToBuyController(service) {
     var toBuy = this;
 
-    toBuy.list = service.items;
-    toBuy.criterion = service.notBought;
-    toBuy.empty = service.allBought;
+    toBuy.list = service.toBuy;
 
-    toBuy.buy = function (id) {
-      service.buy(id);
+    toBuy.buy = function (index) {
+      service.buy(index);
     };
   }
 
@@ -21,50 +19,27 @@
   function AlreadyBoughtController(service) {
     var bought = this;
 
-    bought.list = service.items;
-    bought.criterion = service.bought;
-    bought.empty = service.noneBought;
+    bought.list = service.bought;
   }
 
   function ShoppingListCheckOffService() {
     var service = this;
-
-    service.items = [
-      {id: 1, name: 'Bran', quantity: '1 bag', bought: false},
-      {id: 2, name: 'Carrots', quantity: 8, bought: false},
-      {id: 3, name: 'Celery sticks', quantity: 5, bought: false},
-      {id: 4, name: 'Apples', quantity: 6, bought: false},
-      {id: 5, name: 'Tomatoes', quantity: 4, bought: false},
-      {id: 6, name: 'Orange juice', quantity: '1 carton', bought: false},
-      {id: 7, name: 'Oatmeal', quantity: '2 bags', bought: false},
-      {id: 8, name: '“How to quit cookies”, second edition', quantity: 1, bought: false}
+    service.toBuy = [
+      {name: 'Bran', quantity: '1 bag'},
+      {name: 'Carrots', quantity: 8},
+      {name: 'Celery sticks', quantity: 5},
+      {name: 'Apples', quantity: 6},
+      {name: 'Tomatoes', quantity: 4},
+      {name: 'Orange juice', quantity: '1 carton'},
+      {name: 'Oatmeal', quantity: '2 bags'},
+      {name: '“How to quit cookies”, second edition', quantity: 1}
     ];
+    service.bought = [];
 
-    service.bought = function (item) {
-      return item.bought;
-    };
-
-    service.notBought = function (item) {
-      return !item.bought;
-    };
-
-    service.allBought = function () {
-      return service.items.every(function (item) {
-        return item.bought;
-      });
-    };
-
-    service.noneBought = function () {
-      return service.items.some(function (item) {
-        return item.bought;
-      });
-    };
-
-    service.buy = function (idToBuy) {
-      service.items.forEach(function (item) {
-        if (item.id === idToBuy) {
-          item.bought = true;
-        }
+    service.buy = function (index) {
+      var bought = service.toBuy.splice(index, 1);
+      bought.forEach(function (item) {
+        service.bought.push(item);
       });
     };
   }
